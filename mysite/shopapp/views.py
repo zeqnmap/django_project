e9a50c02c71b4f9e630909f5bb0a97640016b8c8
yaroslavmanko <9276307727@my.lerna.by>
2026-04-05 20:42:52 +0300

    пересобрал структуру

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,7 +1,12 @@
+from django.contrib.auth.models import Group
 from django.shortcuts import render
+from django.http import HttpRequest
 from datetime import datetime
 
-def product_list(request):
+from .models import Product, Order
+
+
+def shop_index(request: HttpRequest):
     products = [
         {
             'name': 'Ноутбук',
@@ -18,9 +23,30 @@ def product_list(request):
         {
             'name': 'Наушники',
             'price': 5000,
-            'description': 'Беспроводные наушники с шумоподавлением',
+            'description': 'Беспроводные наушники',
             'created_at': datetime(2026, 2, 20)
         },
     ]
     context = {'products': products}
     return render(request, 'shopapp/index.html', context=context)
+
+
+def groups_list(request: HttpRequest):
+    context = {
+        "groups": Group.objects.prefetch_related('permissions').all()
+    }
+    return render(request, 'shopapp/groups-list.html', context=context)
+
+
+def products_list(request: HttpRequest):
+    context = {
+        "products": Product.objects.all()
+    }
+    return render(request, 'shopapp/products-list.html', context=context)
+
+
+def orders_list(request: HttpRequest):
+    context = {
+        "orders": Order.objects.select_related("user").prefetch_related("products").all()
+    }
+    return render(request, 'shopapp/orders-list.html', context=context)
