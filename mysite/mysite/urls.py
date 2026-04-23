8ebfaf6469003947e57f612f8ad3b9825547b3ee
yaroslavmanko <9276307727@my.lerna.by>
2026-04-23 23:07:45 +0300

    сделал дз

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shopapp.urls')),
+    path('myauth/', include('myauth.urls')),
 ]
