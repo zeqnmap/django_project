d443ef10fadbbab583b5705ad099f4fe652a7322
yaroslavmanko <9276307727@my.lerna.by>
2026-05-25 23:12:19 +0300

    настроено poetry

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -201,7 +201,7 @@ logging.config.dictConfig({
         'disable_existing_loggers': False,
         'formatters': {
             'console': {
-                'format': '%(asctime)s [%(levelname)s]  [%(name)s:%(lineno)] %(module)s %(message)s '
+                'format': '%(asctime)s [%(levelname)s]  [%(name)s:%(lineno)s] %(module)s %(message)s '
             },
         },
        'handlers': {
